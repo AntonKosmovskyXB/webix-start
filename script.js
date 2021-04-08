d2a3cffc6e44387481c305386caaba1a548b876e
AntonKosmovskyXB <82081078+AntonKosmovskyXB@users.noreply.github.com>
2021-04-08 10:58:25 +0300

    fix mistakes in the code

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-var small_film_set = [
-	{ id:1, title:"The Shawshank Redemption", year:1994, votes:678790, rating:9.2, rank:1, category:"Thriller"},
-	{ id:2, title:"The Godfather", year:1972, votes:511495, rating:9.2, rank:2, category:"Crime"},
-	{ id:3, title:"The Godfather: Part II", year:1974, votes:319352, rating:9.0, rank:3, category:"Crime"},
-	{ id:4, title:"The Good, the Bad and the Ugly", year:1966, votes:213030, rating:8.9, rank:4, category:"Western"},
-	{ id:5, title:"Pulp fiction", year:1994, votes:533848, rating:8.9, rank:5, category:"Crime"},
-	{ id:6, title:"12 Angry Men", year:1957, votes:164558, rating:8.9, rank:6, category:"Western"}
+const small_film_set = [
+	{ title:"The Shawshank Redemption", year:1994, votes:678790, rating:9.2, rank:1, category:"Thriller"},
+	{ title:"The Godfather", year:1972, votes:511495, rating:9.2, rank:2, category:"Crime"},
+	{ title:"The Godfather: Part II", year:1974, votes:319352, rating:9.0, rank:3, category:"Crime"},
+	{ title:"The Good, the Bad and the Ugly", year:1966, votes:213030, rating:8.9, rank:4, category:"Western"},
+	{ title:"Pulp fiction", year:1994, votes:533848, rating:8.9, rank:5, category:"Crime"},
+	{ title:"12 Angry Men", year:1957, votes:164558, rating:8.9, rank:6, category:"Western"}
 ];
 
-webix.ui({
-  rows:[
-    {
-      view:"toolbar",
-      id:"headerToolbar",
-      css:"webix_dark",
-      paddingX: 10,
-      elements: [
-        {view:"label", label:"My App", id:"myAppLabel"},
-        {},
-        {view:"button", type:"icon", icon:"wxi-user", label:"Profile", id:"profileButton", 
-        css: "webix_transparent", width: 100}
-      ]},
+const header = {
+    view:"toolbar",
+    css:"webix_dark",
+    paddingX: 10,
+    elements: [
+      {view:"label", label:"My App"},
+      {},
       {
-        cols:[
-          {
+	view:"button", 
+	type:"icon", 
+	icon:"wxi-user", 
+	label:"Profile", 
+	id:"profileButton", 
+        css: "webix_transparent", 
+	width: 100
+      }
+   ]
+};
+	      
+const main = {
+    cols:[
+        {
             view:"list",
             id:"appList",
             minWidth: 150,
@@ -50,7 +56,7 @@ webix.ui({
             id:"filmsSearchForm",
             width: 300,
             elements: [
-              {template:"EDIT FILMS", type:"section"},
+              {template:"edit films", type:"section"},
               {view:"text", label:"Title", id:"filmTitle"},
               {view:"text", label:"Year", id:"filmYear"},
               {view:"text", label:"Rating", id:"filmRating"},
@@ -60,15 +66,21 @@ webix.ui({
                 {view:"button", value:"Clear", id:"clearTable"}
               ]},
               {}
-            ]
-          },
-      ],
-    },
-    {
-      template:"The software is provided by <a href='https://webix.com' target='_blank'>https://webix.com</a>. All rights reserved (c)", 
-      id:"appFooter", 
-      css:"app-footer", 
-      height: 30
-    },
-  ]
-});
+          ]
+       },
+    ],
+};
+	    
+const footer = {
+    template:"The software is provided by <a href='https://webix.com' target='_blank'>https://webix.com</a>. All rights reserved (c)", 
+    css:"app-footer", 
+    height: 30
+};
+
+ webix.ui({
+     rows: [
+         header,
+         main,
+         footer,
+     ]
+ });
